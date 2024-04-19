2574e585744f6ae398510f8ea1565aa8e3d380e4
Victor Pongolino <victor.pongolino@partner.bmw.com.br>
2024-04-19 12:15:47 -0300

    Smoke tests for product exclusion

--- test/nex-smoketest.sh
+++ test/nex-smoketest.sh
@@ -34,9 +34,22 @@ curl -s -XPOST  "${STD_APP_URL}/products" \
     -H 'Content-Type: application/json' \
     -d '{"id": "the_odyssey", "title": "The Odyssey", "passenger_capacity": 101, "maximum_speed": 5, "in_stock": 10}'
 echo
+
+echo "=== Creating a product id: another_product ==="
+curl -s -XPOST  "${STD_APP_URL}/products" \
+    -H 'accept: application/json' \
+    -H 'Content-Type: application/json' \
+    -d '{"id": "another_product", "title": "Another Product", "passenger_capacity": 10, "maximum_speed": 8, "in_stock": 15}'
+echo
+
 # Test: Get Product
 echo "=== Getting product id: the_odyssey ==="
 curl -s "${STD_APP_URL}/products/the_odyssey" | jq .
+
+# Test: Delete Product
+echo "=== Delete Product id: another_product ==="
+curl -s -XDELETE "${STD_APP_URL}/products/another_product"
+echo
 
 # Test: Create Order
 echo "=== Creating Order ==="
