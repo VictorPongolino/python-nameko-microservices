28831acba51ea6cc9742084f3a798309e3f4b67d
Victor Pongolino <victor.pongolino@partner.bmw.com.br>
2024-04-23 10:52:56 -0300

    Change Redis product exclusion to return the exclusion state

--- products/products/dependencies.py
+++ products/products/dependencies.py
@@ -58,6 +58,7 @@ class StorageWrapper:
         result = self.client.delete(self._format_key(product_id))
         if result == 0:
             raise NotFound(f'Product ID {product_id} does not exist')
+        return result
 
     def decrement_stock(self, product_id, amount):
         return self.client.hincrby(
