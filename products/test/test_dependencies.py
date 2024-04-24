d11cb9902994b59f07cc4ce9d84a84c693e2be1c
Victor Pongolino <victor.pongolino@hotmail.com>
2024-04-24 17:18:55 -0300

    Added unity tests for find products by id (Redis)

--- products/test/test_dependencies.py
+++ products/test/test_dependencies.py
@@ -1,5 +1,5 @@
 import pytest
-from mock import Mock
+from mock import Mock, patch, call
 
 from nameko import config
 from products.dependencies import Storage
@@ -44,7 +44,6 @@ def test_delete_product_fails_not_found(storage, products):
         storage.get(product_id)
     assert f'Product ID {product_id} does not exist' == exc.value.args[0]
 
-
 def test_create(product, redis_client, storage):
 
     storage.create(product)
@@ -57,6 +56,47 @@ def test_create(product, redis_client, storage):
     assert product['passenger_capacity'] == (
         int(stored_product[b'passenger_capacity']))
     assert product['in_stock'] == int(stored_product[b'in_stock'])
+
+
+def test_find_products_by_id(storage):
+    # Mock storage.client.pipeline and storage._from_hash
+    with patch.object(storage.client, 'pipeline', autospec=True) as mock_pipeline, \
+            patch.object(storage, '_from_hash', autospec=True) as mock_from_hash:
+        # Setup the mock for the pipeline
+        pipeline_instance = Mock()
+        mock_pipeline.return_value = pipeline_instance
+
+        # Set up test data
+        product_ids = ['id1', 'id2', 'id3']
+        redis_data = [
+            {'product_id': 'id1', 'title': 'The Odyssey', 'in_stock': '899'},
+            None,  # Simulate a missing product
+            {'product_id': 'id3', 'title': 'The Iliad', 'in_stock': '300'}
+        ]
+
+        # Setup mock behavior for the pipeline
+        pipeline_instance.execute.return_value = redis_data
+
+        # Copying the behaviour of the hgetall as a lambda method
+        # It iterates for each item first and then return the next element of the result of the iteraction.
+        # As the HGETALL can return NONE, it is necessary to check and filter for none elements.
+        # None is returned IF all elements got filtered. That comes from next method.
+        pipeline_instance.hgetall.side_effect = lambda key: next((item for item in redis_data if item), None)
+
+        # Setup mock_from_hash to return the input if it's not None
+        mock_from_hash.side_effect = lambda x: x if x else None
+
+        # Call the method
+        results = storage.find_products_by_id(product_ids)
+
+        # Assertions
+        assert len(results) == 2
+        assert results[0]['product_id'] == 'id1'
+        assert results[1]['product_id'] == 'id3'
+
+        # Check calls to hgetall using call_args_list for ordered calls
+        expected_calls = [call(storage._format_key(pid)) for pid in product_ids]
+        assert pipeline_instance.hgetall.call_args_list == expected_calls
 
 
 def test_decrement_stock(storage, create_product, redis_client):
